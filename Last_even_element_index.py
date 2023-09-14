256ba58336a799c49802755c57f4b5300199e999
Mohammad Adil Khan <119918785+Mohammad-Adilkhan3@users.noreply.github.com>
2023-09-14 23:18:20 +0530

    def find_last_even_index(arr):     for i in range(len(arr) - 1, -1, -1):         if arr[i] % 2 == 0:             return i     # If no even element is found, you can return -1 or raise an exception.     return -1
    # Example usage:
    n=int(input())
    l=list(map(int,input().split()))
    last_even_index = find_last_even_index(l)
    
    if last_even_index != -1:
        print(last_even_index)
    else:
        print("No even element found in the list.")

---
+++ Last_even_element_index.py
@@ -0,0 +1,16 @@
+def find_last_even_index(arr):
+    for i in range(len(arr) - 1, -1, -1):
+        if arr[i] % 2 == 0:
+            return i
+    # If no even element is found, you can return -1 or raise an exception.
+    return -1
+
+# Example usage:
+n=int(input())
+l=list(map(int,input().split()))
+last_even_index = find_last_even_index(l)
+
+if last_even_index != -1:
+    print(last_even_index)
+else:
+    print("No even element found in the list.")
